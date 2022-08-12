4d868a21e0ab2a5912c9ed740614b2bf5fdae75f
Darya <daryalas17@gmail.com>
2022-08-12 19:17:00 +0300

    s62

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите числа через пробел ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int Kol(int[] array)
+{
+  int j = 0;
+
+   for (int i = 0; i < array.Length; i++)
+   {
+       if ( array[i] > 0) j = j + 1; 
+   }
+  return j;
+}
+
+
+Console.WriteLine($"Количетво элементов больше 0: {Kol(arr)}");
